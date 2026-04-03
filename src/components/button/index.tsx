0842811f9961a7503702ab33165edf6ea514d1a2
Felipe Santiago Morais <fillikz@gmail.com>
2026-04-03 10:11:16 -0300

    feat(button): configure base button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,25 @@
+import { PropsWithChildren } from "react";
+import styles from "./index.module.scss";
+import cn from "classnames";
+import { IconBase, IconBaseProps, IconContext, IconType } from "react-icons";
+
+type IButtonProps = PropsWithChildren & {
+  buttonStyle?: "primary" | "secondary" | "tertiary" | "outline" | "link";
+  iconLeft?: IconType;
+  iconRight?: IconType;
+};
+
+export const Button = ({
+  children,
+  buttonStyle,
+  iconLeft: IconLeft,
+  iconRight: IconRight,
+}: IButtonProps) => {
+  return (
+    <button className={cn(styles.buttonBase, buttonStyle && styles[buttonStyle])}>
+      {IconLeft && <IconLeft className={styles.iconLeft} />}
+      {children}
+      {IconRight && <IconRight className={styles.iconRight} />}
+    </button>
+  );
+};
